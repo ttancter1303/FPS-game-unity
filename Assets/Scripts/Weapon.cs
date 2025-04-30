def5e54399d74690399e6041edb832c484bd0d73
ttancter1303 <01689674504zx@gmail.com>
2025-04-30 15:01:48 +0700

    add animation shoot and muzzle flash

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int damageAmount = 1;
+    [SerializeField] Animator animator;
+    [SerializeField] GameObject HitVFXPrefab;
 
     StarterAssetsInputs starterAssetsInputs;
+    const string SHOOT_STRING = "Shoot";
+    const string IDLE_STRING = "Idle";
 
     void Awake()
     {
@@ -21,14 +26,20 @@ public class Weapon : MonoBehaviour
     {
         if (!starterAssetsInputs.shoot) return;
 
+        muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
+
         RaycastHit hit;
+            
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
+
+            Instantiate(HitVFXPrefab, hit.point, Quaternion.identity);  
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(damageAmount);
 
-            starterAssetsInputs.ShootInput(false);
         }
     }
 }
