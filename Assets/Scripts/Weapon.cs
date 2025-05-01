01b74c0c959db2405cf0c0710d41b407a0e4c8d1
ttancter1303 <01689674504zx@gmail.com>
2025-05-01 15:51:37 +0700

    add robot anim

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,16 +1,17 @@
 using StarterAssets;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] GameObject hitVFXPrefab;
+    [SerializeField] Animator animator;
     [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int damageAmount = 1;
-    [SerializeField] Animator animator;
-    [SerializeField] GameObject HitVFXPrefab;
 
     StarterAssetsInputs starterAssetsInputs;
+
     const string SHOOT_STRING = "Shoot";
-    const string IDLE_STRING = "Idle";
 
     void Awake()
     {
@@ -31,15 +32,13 @@ public class Weapon : MonoBehaviour
         starterAssetsInputs.ShootInput(false);
 
         RaycastHit hit;
-            
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
+            Instantiate(hitVFXPrefab, hit.point, quaternion.identity);
 
-            Instantiate(HitVFXPrefab, hit.point, Quaternion.identity);  
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(damageAmount);
-
         }
     }
 }
