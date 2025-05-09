a69164ab3b6370beeb5af03288e89df47bf83285
Tạ Tuấn Anh <01689674504zx@gmail.com>
2025-05-09 17:12:11 +0700

    add robot explosion

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    [SerializeField] int startingHealth = 3;
+    [SerializeField] int startingHealth = 10;
+    [SerializeField] ParticleSystem particle;
 
     int currentHealth;
 
@@ -17,7 +18,9 @@ public class EnemyHealth : MonoBehaviour
 
         if (currentHealth <= 0)
         {
+            Instantiate(particle, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
+            
         }
     }
 }
