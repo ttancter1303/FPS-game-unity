a4c7c87911cb659e31f0732e3dc23400797388c9
Tạ Tuấn Anh <01689674504zx@gmail.com>
2025-05-10 17:23:23 +0700

    add health bar

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,11 +1,15 @@
 using Cinemachine;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int startingHealth = 100;
     [SerializeField] CinemachineVirtualCamera deathVirtualCamera;
     [SerializeField] Transform weaponCamera;
+    [SerializeField] TMP_Text healthText;
+    [SerializeField] Image[] healthBar;
 
     int currentHealth;
     int gameOverVirtualCameraPriority = 20;
@@ -13,18 +17,37 @@ public class PlayerHealth : MonoBehaviour
     void Awake()
     {
         currentHealth = startingHealth;
+        AdjustHealthUI();
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
-        Debug.Log("Take damage: "+amount);  
-
+        AdjustHealthUI();
+        healthText.text = currentHealth.ToString();
         if (currentHealth <= 0)
         {
+            healthText.text = "0";
             weaponCamera.parent = null;
             deathVirtualCamera.Priority = gameOverVirtualCameraPriority;
             Destroy(gameObject);
         }
+    }
+    public void AdjustHealthUI()
+    {
+        for (int i = 0; i < healthBar.Length; i++)
+        {
+            if (i < (currentHealth / 10))
+            {
+                healthBar[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                healthBar[i].gameObject.SetActive(false);
+            }
+
+        }
+      
+
     }
 }
