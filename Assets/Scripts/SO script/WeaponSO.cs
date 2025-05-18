491effb26c8f7c9fe99dbbd71716e9997f05bc10
Tạ Tuấn Anh <01689674504zx@gmail.com>
2025-05-18 19:36:26 +0700

    config new map

--- Assets/Scripts/SO script/WeaponSO.cs
+++ Assets/Scripts/SO script/WeaponSO.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WeaponSO", menuName = "Scriptable Objects/WeaponSO")]
 public class WeaponSO : ScriptableObject
 {
+    public string WeaponID;
     public Sprite weaponIcon;
     public GameObject HitVFXPrefab;
     public GameObject WeaponPrefab;
